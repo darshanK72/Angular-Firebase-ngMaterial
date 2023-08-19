417a7c25e57b64df3987cba77c48d2f11201dd03
Darshan Khairnar <darshankhairnar72@gmail.com>
2023-08-19 16:51:12 +0530

    new binding and directives project

--- 1. AngularFirstApp/src/app/app.module.ts
+++ 1. AngularFirstApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { MainComponent } from './Layout/main/main.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+import { SidebarComponent } from './Layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
