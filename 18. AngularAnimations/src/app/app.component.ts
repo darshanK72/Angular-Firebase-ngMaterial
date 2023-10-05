6d74f8be09fd4c964d2cf6d9db572989773161d3
Darshan Khairnar <darshankhairnar72@gmail.com>
2023-10-05 15:13:16 +0530

    added route animations

--- 18. AngularAnimations/src/app/app.component.ts
+++ 18. AngularAnimations/src/app/app.component.ts
@@ -1,10 +1,78 @@
+import {
+  animate,
+  group,
+  query,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('onoff', [
+      state(
+        'on',
+        style({
+          backgorundColor: 'white',
+        })
+      ),
+      state(
+        'off',
+        style({
+          backgroundColor: 'black',
+        })
+      ),
+      transition('off => on', [animate('1s')]),
+      transition('on => off', [animate('1s')]),
+    ]),
+    trigger('routeAnimations', [
+      transition('* <=> *', [
+        query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+          optional: true,
+        }),
+        group([
+          query(
+            ':enter',
+            [
+              style({ transform: 'translateX(100%)' }),
+              animate(
+                '0.5s ease-in-out',
+                style({ transform: 'translateX(0%)' })
+              ),
+            ],
+            { optional: true }
+          ),
+          query(
+            ':leave',
+            [
+              style({ transform: 'translateX(0%)' }),
+              animate(
+                '0.5s ease-in-out',
+                style({ transform: 'translateX(100%)' })
+              ),
+            ],
+            { optional: true }
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'AngularAnimations';
+
+  value = 'off';
+
+  onClick() {
+    if (this.value == 'on') {
+      this.value = 'off';
+    } else {
+      this.value = 'on';
+    }
+  }
 }
